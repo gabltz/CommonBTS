8907eec8b8235ddfa410480450d6dd50d7a216b3
Gabin LOTZ <pro.agentgeek88@gmail.com>
2024-05-28 16:27:05 +0200

    Update script.js

--- HTML/CV/script.js
+++ HTML/CV/script.js
@@ -16,7 +16,23 @@ document.addEventListener('DOMContentLoaded', function() {
     sections.forEach(section => {
         observer.observe(section);
     });
+    // Smooth scrolling for nav links
+
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop - document.querySelector('header').offsetHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
 });
+
+
 
 // Smooth scrolling for mouse wheel
 (function() {
